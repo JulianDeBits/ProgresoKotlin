fee17d06b6da2d058140dea4bb5f0ab6652777d0
JulianDeBits <julianquinteroortiz@gmail.com>
2025-03-13 23:20:40 -0500

    Avances Interfaces

--- app/src/main/java/com/example/manejocomponentes/MainActivity.kt
+++ app/src/main/java/com/example/manejocomponentes/MainActivity.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.manejocomponentes.ui.theme.ManejoComponentesTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.res.colorResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +27,19 @@ class MainActivity : ComponentActivity() {
 
     fun CambiarTexto (Componente: TextView){
 
-        if (Componente.text == "Ahora la Opcion 2"){
+        if (Componente.text == "Te amo Princesa"){
 
-            Componente.text = "Mostrando Opcion 1"
+            Componente.text = "Muestre el inicio"
 
         } else {
 
-            Componente.text = "Ahora la Opcion 2"
+            Componente.text = "Te amo Princesa"
         }
+
+    }
+
+    fun CambiarPantalla(){
+
 
     }
 }
